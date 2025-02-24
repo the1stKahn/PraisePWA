2ac8d7216b20b05c51f3028545f695779cd8b81e
the1stKahn <igwenwokedicanada@gmail.com>
2025-02-24 00:05:17 -0500

    fourth Commit

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,12 @@
 const CACHE_NAME = 'to-do-pwa-cache-v1';
 const FILES_TO_CACHE = [
-    '/PraisePWA/',
-    '/PraisePWA/index.html',
-    '/PraisePWA/style.css',
-    '/PraisePWA/app.js',
-    '/PraisePWA/manifest.json',
-    '/PraisePWA/icons/icon-128.png',
-    '/PraisePWA/icons/icon-512.png'
+    'PraisePWA',
+    'PraisePWA/index.html',
+    'PraisePWA/style.css',
+    'PraisePWA/app.js',
+    'PraisePWA/manifest.json',
+    'PraisePWA/icons/icon-128.png',
+    'PraisePWA/icons/icon-512.png'
 ];
 self.addEventListener('install', (event) => {
     event.waitUntil(
